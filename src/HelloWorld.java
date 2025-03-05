c9f76baf30a5302b50e364c52c4f128b5a038af5
isaac <isaaccrvg98@hotmail.com>
2025-03-05 15:49:27 -0300

    exercício 7

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,15 +1,8 @@
+import java.time.LocalDate;
+
 public class HelloWorld{
     public static void main (String[] args){
-
-        String nome = "isaac";
-        String cidade = "Rio de Janeiro";
-        int idade = 22;
-        double salario = 10000.00;
-
-
-        System.out.println("Oi, me chamo: " + nome + ", sou estudante de Engenharia de software!");
-        System.out.println("Nascido em: " + cidade);
-        System.out.println("idade: " + idade);
-        System.out.println("Meu salário é de: R$" + salario);
+        System.out.println("Oi, meu nome é isaac, sou estudante de Engenharia de software!");
+        System.out.println("Data de hoje: " + LocalDate.now());
     }
 }
