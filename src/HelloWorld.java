ea94c3261725fc0088c84e7bb3ddcef3ebd4b0f7
isaac <isaaccrvg98@hotmail.com>
2025-03-05 15:51:16 -0300

    exercício 6

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,5 @@
-import java.time.LocalDate;
-
 public class HelloWorld{
     public static void main (String[] args){
-        System.out.println("Oi, meu nome é isaac, sou estudante de Engenharia de software!");
-        System.out.println("Data de hoje: " + LocalDate.now());
+        System.out.println("Hello, World!");
     }
 }
